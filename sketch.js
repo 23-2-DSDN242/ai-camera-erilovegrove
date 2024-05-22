c5ec474f137b8aaabd330840273d61eb9bb469bb
erilovegrove <140354030+erilovegrove@users.noreply.github.com>
2024-05-23 10:38:45 +1200

    d

--- sketch.js
+++ sketch.js
@@ -202,14 +202,113 @@
 
 
 
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+// let sourceImg=null;
+// let maskImg=null;
+// let renderCounter=0;
+// let curLayer = 0;
+
+// // change these three lines as appropiate
+// let sourceFile = "input_2.jpg";
+// let maskFile   = "mask_2.png";
+// let outputFile = "output_1.png";
+
+// function preload() {
+//   sourceImg = loadImage(sourceFile);
+//   maskImg = loadImage(maskFile);
+// }
+
+// function setup () {
+//   let main_canvas = createCanvas(1920, 1080);
+//   main_canvas.parent('canvasContainer');
+
+//   imageMode(CENTER);
+//   noStroke();
+//   background(0, 0, 128);
+//   sourceImg.loadPixels();
+//   maskImg.loadPixels();
+//   colorMode(HSB);
+// }
+
+// function draw () {
+//   if (curLayer == 0) {
+//     let num_lines_to_draw = 40;
+//     // get one scanline
+//     for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+//       for(let i=0; i<1920; i++) {
+//         colorMode(RGB);
+//         let pix = sourceImg.get(i, j);
+//         // create a color from the values (always RGB)
+//         let col = color(pix);
+//         let mask = maskImg.get(i, j);
+
+//         colorMode(HSB, 360, 100, 100);
+//         // draw a "dimmed" version in gray
+//         let h = hue(col);
+//         let s = saturation(col);
+//         let b = brightness(col);
+
+//         let new_brt = map(b, 0, 100, 30, 50);
+//         let new_col = color(h, 0, new_brt);
+//         set(i, j, new_col);
+//       }
+//     }
+//     renderCounter = renderCounter + num_lines_to_draw;
+//     updatePixels();
+//   }
+//   else {
+//     rectMode(CORNERS);
+//     for(let i=0; i<200; i++) {
+//       let x1 = random(0, width);
+//       let y1 = random(0, height);
+//       let x2 = x1 + random(-10, 10);
+//       let y2 = y1 + random(-10, 10);
+//       colorMode(RGB);
+//       let pix = sourceImg.get(x1, y1);
+//       let mask = maskImg.get(x1, y1);
+//       let col = color(pix);
+//       stroke(col);
+//       fill(col);
+//       if(mask[1] < 128) {
+//         line(x1, y1, x2, y2);
+//       }
+//       else {
+//         rect(x1, y1, x2, y2);
+//       }
+//     }
+//     renderCounter = renderCounter + 1;
+//     // set(i, j, new_col);
+//   }
+//   // print(renderCounter);
+//   if(curLayer == 0 && renderCounter > 1080) {
+//     curLayer = 1;
+//     renderCounter = 0;
+//   }
+//   else if(curLayer == 1 && renderCounter > 500) {
+//     console.log("Done!")
+//     noLoop();
+//     // uncomment this to save the result
+//     // saveArtworkImage(outputFile);
+//   }
+// }
+
+// function keyTyped() {
+//   if (key == '!') {
+//     saveBlocksImages();
+//   }
+// }
+
+
+
+
+
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 let curLayer = 0;
 
-// change these three lines as appropiate
+// change these three lines as appropriate
 let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
+let maskFile = "mask_2.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -217,7 +316,7 @@ function preload() {
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 1080);
   main_canvas.parent('canvasContainer');
 
@@ -229,12 +328,12 @@ function setup () {
   colorMode(HSB);
 }
 
-function draw () {
+function draw() {
   if (curLayer == 0) {
     let num_lines_to_draw = 40;
     // get one scanline
-    for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
-      for(let i=0; i<1920; i++) {
+    for (let j = renderCounter; j < renderCounter + num_lines_to_draw && j < 1080; j++) {
+      for (let i = 0; i < 1920; i++) {
         colorMode(RGB);
         let pix = sourceImg.get(i, j);
         // create a color from the values (always RGB)
@@ -254,37 +353,43 @@ function draw () {
     }
     renderCounter = renderCounter + num_lines_to_draw;
     updatePixels();
-  }
-  else {
+  } else {
     rectMode(CORNERS);
-    for(let i=0; i<200; i++) {
+    for (let i = 0; i < 200; i++) {
       let x1 = random(0, width);
       let y1 = random(0, height);
-      let x2 = x1 + random(-10, 10);
-      let y2 = y1 + random(-10, 10);
       colorMode(RGB);
       let pix = sourceImg.get(x1, y1);
       let mask = maskImg.get(x1, y1);
       let col = color(pix);
       stroke(col);
-      fill(col);
-      if(mask[1] < 128) {
-        line(x1, y1, x2, y2);
-      }
-      else {
+      if (mask[1] < 128) {
+        // Draw the rectangle as before if needed
+        let x2 = x1 + random(-10, 10);
+        let y2 = y1 + random(-10, 10);
+        fill(col);
         rect(x1, y1, x2, y2);
+
+
+
+
+      } else {
+       let crossSize = random(5, 15);
+        // Draw horizontal line of the cross
+        line(x1 - crossSize, y1, x1 + crossSize, y1);
+        // Draw vertical line of the cross
+        line(x1, y1 - crossSize, x1, y1 + crossSize);
+        
       }
     }
     renderCounter = renderCounter + 1;
-    // set(i, j, new_col);
   }
   // print(renderCounter);
-  if(curLayer == 0 && renderCounter > 1080) {
+  if (curLayer == 0 && renderCounter > 1080) {
     curLayer = 1;
     renderCounter = 0;
-  }
-  else if(curLayer == 1 && renderCounter > 500) {
-    console.log("Done!")
+  } else if (curLayer == 1 && renderCounter > 500) {
+    console.log("Done!");
     noLoop();
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
